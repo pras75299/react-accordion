ea89e0e8f05df700df708138aa079f65d7463b5a
pras75299 <singh.prashantking@gmail.com>
2021-04-16 13:37:09 +0530

    complete questions accordion component

--- src/Question.js
+++ src/Question.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 const Question = ({title, info}) => {
+  const[showinfo, setShowInfo] = useState(false);
   return <article className="question">
       <header>
         <h4>{title}</h4>
-        <button className="btn">btn</button>
+        <button className="btn" onClick={() => setShowInfo(!showinfo)}>
+          {showinfo ? <AiOutlineMinus/> : <AiOutlinePlus/>}
+        </button>
       </header>
-
+      {showinfo && <p>{info}</p>}
   </article>;
 };
 
