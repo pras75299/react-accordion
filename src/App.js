a7a392ce7c2d68238479b1b51208a0682579e956
pras75299 <singh.prashantking@gmail.com>
2021-04-16 13:24:12 +0530

    show questions and correct app js questions looping logic

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
     <div className="container">
       <h3>Questions and answers about login</h3>
       <section className="info">
-        {questions.map(() => {
-          return <SingleQuestion key={questions.id} {...questions}/>
+        {questions.map((question) => {
+          return <SingleQuestion key={question.id} {...question}/>
         })}
       </section>
     </div>
